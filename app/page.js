7379582139ab1b9fbb685fa527c9c1b5ea0eafc4
Andrew Stein <31190331+ABStein@users.noreply.github.com>
2024-03-06 08:34:22 -0600

    add some flare

--- app/page.js
+++ app/page.js
@@ -1,22 +1,16 @@
 import Link from 'next/link';
 import { getPosts, getPostBySlug } from './lib/initSupabase';
-
-export const preload = async ({ params: { slug } }) => {
-    const post = await getPostBySlug(slug);
-    const postsData = await getPosts();
-    return { post, postsData };
-}
+import AnimatedComponent from './components/AnimatedComponent';
 
 export default async function HomePage() {
-    const postsData = await getPosts();
+    const posts = await getPosts();
     return (
         <div className='lg:pt-12 pt-8'>
             <div className='responsive-content-width'>
                 <div className='lg:px-24 px-6'>
                     <div className='w-10/12 mx-auto'>
                         <h1 className='md:text-center lg:text-6xl text-3xl text-gray-900'>
-                            Hey there! I'm{' '}
-                            <span className='font-bold text-indigo-500'>Andrew</span>.
+                            Hey there! I'm <AnimatedComponent word='Andrew.' />
                         </h1>
                         <p className='md:text-left lg:text-4xl md:text-2xl sm:text-xl text-gray-900 md:pt-6 pt-3'>
                             a Web Developer from Chicago, Illinois. I am passionate about building accessible, performant, and responsive web experiences, but I am even more passionate about growing and learning on this journey through the wild world of web development.
@@ -27,7 +21,7 @@ export default async function HomePage() {
                         <div className='w-10/12 mx-auto'>
                             <h2 className='lg:text-2xl font-bold my-2'>Recently posted</h2>
                             <div className='mt-6 mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4'>
-                                {postsData.slice(0, 3).map((post) => (
+                                {posts.slice(0, 3).map((post) => (
                                     <div key={post.id} className='p-8 shadow-2xl shadow-gray-900 rounded-2xl lg:hover:-translate-y-6 nav-link'>
                                         <Link href={`/blog/${post.slug}/`}>
                                             <h2 className='text-xl font-semibold'>{post.title}</h2>
