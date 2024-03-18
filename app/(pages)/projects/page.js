1761deb1591fb5beab8e5aa913bd6f662643b10f
Andrew Stein <31190331+ABStein@users.noreply.github.com>
2024-03-18 16:31:39 -0500

    also remove projects until its finished

--- app/(pages)/projects/page.js
+++
@@ -1,9 +0,0 @@
-export default function Projects() {
-    return (
-        <section className='lg:pt-12 pt-8 pb-40'>
-            <div className='responsive-content-width'>
-                <h1>Projects</h1>
-            </div>
-        </section>
-    );
-}
